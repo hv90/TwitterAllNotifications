8a5bef0537dd2e1dc3c302e7cf3b2b7f42fa0785
hv90 <hugo.machado@hi.com.br>
2021-05-10 02:14:58 -0300

    finish headerString

--- App.js
+++ App.js
@@ -6,16 +6,6 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const oauth = {
-    status: encodeURIComponent("say hello, to all the apples on the ground"),
-    oauth_token: encodeURIComponent(env.USER_ACCESS_TOKEN),
-    oauth_consumer_key: encodeURIComponent(env.RN_APP_API_KEY),
-  };
-
-  const signingKey = `${encodeURIComponent(
-    env.RN_APP_API_SECRET_KEY
-  )}&${encodeURIComponent(env.USER_ACCESS_TOKEN_SECRET)}`;
-
   const signatureBaseBuilder = () => {
     const base_url = "https://api.twitter.com/1.1/";
 
@@ -26,14 +16,54 @@ export default function App() {
       .map(([key, value]) => (stringParam += `${key}=${value}&`));
 
     stringParam = stringParam.slice(0, -1);
+
     return `POST&${encodeURIComponent(base_url)}&${encodeURIComponent(
       stringParam
     )}`;
   };
 
-  const signatureBase = signatureBaseBuilder();
+  const randomStringBuilder = (length, chars) => {
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      result += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return result;
+  };
+  const randomString = randomStringBuilder(
+    32,
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  );
 
-  const signature = base64.stringify(hmacSha1(signatureBase, signingKey));
+  const headerStringBuilder = () => {
+    let str = "Oauth ";
+    Object.entries(oauth)
+      .sort()
+      .map(([key, value]) => (str += `${key}="${value}", `));
+    console.log(str);
+
+    return str.slice(0, -2);
+  };
+
+  const oauth = {
+    oauth_token: env.USER_ACCESS_TOKEN,
+    oauth_consumer_key: env.RN_APP_API_KEY,
+    oauth_nonce: randomString,
+    oauth_signature_method: "HMAC-SHA1",
+    oauth_timestamp: `${new Date().getTime()}`,
+    oauth_version: "1.0",
+  };
+
+  const signingKey = `${encodeURIComponent(
+    env.RN_APP_API_SECRET_KEY
+  )}&${encodeURIComponent(env.USER_ACCESS_TOKEN_SECRET)}`;
+
+  const oauth_signature = base64.stringify(
+    hmacSha1(signatureBaseBuilder(), signingKey)
+  );
+
+  oauth["oauth_signature"] = oauth_signature;
+
+  console.log(headerStringBuilder());
 
   return (
     <View style={styles.container}>
